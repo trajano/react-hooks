bb9c77855d50f22447699b8c14df3a8240e23111
Archimedes Trajano <developer@trajano.net>
2021-11-29 09:54:42 -0500

    Added useDeepState

--- src/useDeepState.ts
+++
@@ -1,8 +0,0 @@
-import { equals } from 'ramda';
-import { Dispatch, useReducer } from 'react';
-export function useDeepState<S>(initialState: S): [S, Dispatch<S>] {
-  return useReducer(
-    (state: S, newState: S) => (equals(state, newState) ? state : newState),
-    initialState
-  );
-}
