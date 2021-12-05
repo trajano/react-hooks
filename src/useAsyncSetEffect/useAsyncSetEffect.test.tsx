642d09e2728e98229a5ba8412e31791f04a64848
Archimedes Trajano <developer@trajano.net>
2021-12-05 08:31:48 -0500

    Added unmount test

--- src/useAsyncSetEffect/useAsyncSetEffect.test.tsx
+++ src/useAsyncSetEffect/useAsyncSetEffect.test.tsx
@@ -5,6 +5,7 @@ import { render } from '@testing-library/react';
 import React, { useState } from 'react';
 import { waitFor } from "@testing-library/dom";
 import { useAsyncSetEffect } from "./useAsyncSetEffect";
+import { delay } from '../delay';
 
 describe("useAsyncSetEffect", () => {
   it("should work", async () => {
@@ -19,4 +20,49 @@ describe("useAsyncSetEffect", () => {
     await waitFor(() => expect(element.textContent).toEqual("bar"));
     await waitFor(() => expect(element.textContent).toEqual("foo"));
   })
+
+  it("should work with delay", async () => {
+    jest.useFakeTimers();
+    function MyComponent() {
+      const [foo, setFoo] = useState("bar");
+
+      useAsyncSetEffect(async () => {
+        await delay(10000);
+        return Promise.resolve("foo")
+      }, setFoo, []);
+      return (<div data-testid="test">{foo}</div>);
+    }
+    const { getByTestId } = render(<MyComponent />)
+    const element = getByTestId("test");
+    await waitFor(() => expect(element.textContent).toEqual("bar"));
+    jest.runAllTimers();
+    await waitFor(() => expect(element.textContent).toEqual("foo"));
+  })
+
+  it("should not call state when umounted", async () => {
+    jest.useFakeTimers();
+    const callback = jest.fn();
+    function MyComponent() {
+      const [foo] = useState("bar");
+
+      useAsyncSetEffect(async () => {
+        await delay(10000);
+        return Promise.resolve("foo")
+      }, callback, []);
+      return (<div data-testid="test">{foo}</div>);
+    }
+    const { getByTestId, unmount } = render(<MyComponent />)
+    const element = getByTestId("test");
+    await waitFor(() => expect(element.textContent).toEqual("bar"));
+    await waitFor(() => expect(callback).toBeCalledTimes(0));
+    unmount();
+    jest.runAllTimers();
+    await waitFor(() => expect(element.textContent).toEqual("bar"));
+    await waitFor(() => expect(callback).toBeCalledTimes(0));
+  })
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
 });
