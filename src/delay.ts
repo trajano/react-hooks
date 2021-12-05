b75deb1cad6e80afe1b5b95d3753f7970ed768b8
Archimedes Trajano <developer@trajano.net>
2021-12-05 08:51:08 -0500

    Added doc about delay

--- src/delay.ts
+++ src/delay.ts
@@ -1,3 +1,7 @@
-export async function delay(ms: number) {
+/**
+ * Utility function to generate a delay.  Used only for testing and is not exported.
+ * @param ms milliseconds to wait
+ */
+ export async function delay(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
