7ad7889273a6bf6450ba76a993f5ba5a330c573a
Archimedes Trajano <developer@trajano.net>
2021-12-10 19:57:44 -0500

    added debounce test

---
+++ src/debounce.test.ts
@@ -0,0 +1,43 @@
+import debounce from "lodash.debounce";
+describe("debounce", () => {
+  beforeEach(() => {
+    jest.useFakeTimers("modern");
+  });
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+  it("should work properly", () => {
+    const callback = jest.fn();
+    const debounced = debounce(callback, 500);
+    debounced();
+    expect(callback).not.toBeCalled();
+
+    jest.advanceTimersByTime(100);
+    debounced();
+    expect(callback).not.toBeCalled();
+
+    jest.advanceTimersByTime(499);
+    expect(callback).not.toBeCalled();
+
+    jest.advanceTimersByTime(1);
+    expect(callback).toBeCalledTimes(1);
+  });
+
+  it("should fire with lead", () => {
+    const callback = jest.fn();
+    const debounced = debounce(callback, 500, { leading: true });
+    expect(callback).not.toBeCalled();
+    debounced();
+    expect(callback).toBeCalledTimes(1);
+
+    jest.advanceTimersByTime(100);
+    debounced();
+    expect(callback).toBeCalledTimes(1);
+
+    jest.advanceTimersByTime(499);
+    expect(callback).toBeCalledTimes(1);
+
+    jest.advanceTimersByTime(1);
+    expect(callback).toBeCalledTimes(2);
+  });
+});
