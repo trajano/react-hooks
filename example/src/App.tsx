13a27ebcecd00b24fac447dba9f9f69d098b0523
Archimedes Trajano <developer@trajano.net>
2022-01-23 18:26:16 -0500

    Expanded example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,10 @@
 import { useClock, useDeepState } from '@trajano/react-hooks';
+import { useState } from 'react';
 export function App(): JSX.Element {
-  useClock();
+  const { useSubscribeEffect } = useClock();
+  const [date, setDate] = useState<number>(Date.now());
+  useSubscribeEffect(setDate)
+
   useDeepState("foo");
-  return <div>Hello world</div>
+  return <div>Hello world {date}</div>
 }
