df300a8058eec6ef115b775f8f1abfdffa9ae2e3
Archimedes Trajano <developer@trajano.net>
2022-01-24 01:07:24 -0500

    specify type

--- src/usePolling/usePolling.ts
+++ src/usePolling/usePolling.ts
@@ -4,8 +4,8 @@ import { usePollingIf } from "./usePollingIf";
  * @param interval milliseconds between calls to the asyncFunction, defaults to a minute
  * @param immediate if true it will run the asyncFunction immediately before looping
  */
-export function usePolling(
-  asyncFunction: () => any | PromiseLike<any>,
+export function usePolling<T = unknown>(
+  asyncFunction: () => T | PromiseLike<T>,
   interval = 60000,
   immediate = true
 ) : void {
