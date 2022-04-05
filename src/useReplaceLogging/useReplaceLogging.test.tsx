a1225caf6facc9afa1d42ffb4e2f1f41b1943a50
Archimedes Trajano <developer@trajano.net>
2022-04-05 03:07:43 -0400

    expanded tests

--- src/useReplaceLogging/useReplaceLogging.test.tsx
+++ src/useReplaceLogging/useReplaceLogging.test.tsx
@@ -36,4 +36,63 @@ describe('useReplaceLogging', () => {
     console.log('This should be shown on the console');
     expect(logCallback.mock.calls).toEqual([['click']]);
   });
+  it('should work with no configuration', async () => {
+
+    function MyComponent() {
+      useReplaceLogging({      });
+
+      return (
+        <button
+          data-testid="test"
+        >
+          Hello world
+        </button>
+      );
+    }
+
+    const {  unmount } = render(<MyComponent />);
+    unmount();
+  });
+
+  it('swap all', async () => {
+    const debugCallback = jest.fn();
+    const logCallback = jest.fn();
+    const infoCallback = jest.fn();
+    const warnCallback = jest.fn();
+    const errorCallback = jest.fn();
+
+    function MyComponent() {
+      useReplaceLogging({
+        log: logCallback,
+        debug: debugCallback,
+        info: infoCallback,
+        warn: warnCallback,
+        error: errorCallback,
+      });
+
+      return (
+        <button
+          data-testid="test"
+          onClick={() => {
+            console.debug('click');
+            console.log('click');
+            console.info('click');
+            console.warn('click');
+            console.error('click');
+          }}
+        >
+          Hello world
+        </button>
+      );
+    }
+
+    const { getByTestId, unmount } = render(<MyComponent />);
+    getByTestId('test').click();
+    expect(debugCallback.mock.calls).toEqual([['click']]);
+    expect(logCallback.mock.calls).toEqual([['click']]);
+    expect(infoCallback.mock.calls).toEqual([['click']]);
+    expect(warnCallback.mock.calls).toEqual([['click']]);
+    expect(errorCallback.mock.calls).toEqual([['click']]);
+  });
+
 });
