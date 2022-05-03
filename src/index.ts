7c5c20cfcc1f1c68f745c2d079c2fecea6619567
Archimedes Trajano <developer@trajano.net>
2022-05-03 00:24:17 -0400

    Added XyzProvider code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 /**
  * @module
  */
-export * from './useAsyncSetEffect';
-export * from './useClock';
-export * from './useStates';
-export * from './useMounted';
-export * from './usePolling';
-export * from './useReplaceLogging';
-export * from './useSubscription';
+export * from "./useAsyncSetEffect";
+export * from "./useClock";
+export * from "./useStates";
+export * from "./useMounted";
+export * from "./usePolling";
+export * from "./useReplaceLogging";
+export * from "./useSubscription";
+export * from "./xyz";
