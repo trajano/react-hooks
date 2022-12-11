516b54042b3552027039f07190f9eb68de3acd62
Archimedes Trajano <developer@trajano.net>
2022-12-11 12:35:06 -0500

    Added useTimeoutEffect

--- src/useTimeoutEffect/useTimeoutEffect.ts
+++ src/useTimeoutEffect/useTimeoutEffect.ts
@@ -1,5 +1,11 @@
 import { useEffect, useRef } from "react";
-
+/**
+ * This hook wraps `setTimeout` to trigger an effect of invoking the callback function when the timeout hits.
+ * This will clear the timeout if the component containing the hook is unmounted.
+ * @param callback callback
+ * @param ms time before callback fires
+ * @param args arguments passed to the callback
+ */
 export function useTimeoutEffect<TArgs extends any[]>(
   callback: (...args: TArgs) => void,
   ms?: number,
