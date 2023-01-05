ccde1397eb0c4faa327ab6bd72571787f2dd7133
Archimedes Trajano <developer@trajano.net>
2023-01-05 07:46:25 -0500

    Yarn lint

--- src/xyz/XyzProvider.tsx
+++ src/xyz/XyzProvider.tsx
@@ -1,13 +1,12 @@
+import { useMemo } from "react";
 import { Context } from "./context";
-import { XyzProviderProps } from "./XyzProviderProps";
-import React, { useMemo } from "react";
 import { XyzContext } from "./XyzContext";
+import { XyzProviderProps } from "./XyzProviderProps";
 
 /**
  * Provides the context to the React application.
  * @param props provider initalization props
  * @return context provider.
- *
  */
 export function XyzProvider({
   children,
