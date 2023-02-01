f81248f8ec3182aed78461bbe31ea4ba6c256d21
Archimedes Trajano <developer@trajano.net>
2023-02-01 14:03:45 -0500

    test: additional react hook tests

--- src/__tests__/useStateBatchTest.tsx
+++ src/__tests__/useStateBatchTest.tsx
@@ -57,11 +57,11 @@ describe("useState batch scenario", () => {
         setBar("blah");
       }, [])
 
-      useEffect(()=> {
+      useEffect(() => {
         noop(foo);
         noop(bar);
         effectFn();
-      },[foo,bar]);
+      }, [foo, bar]);
       renderFn();
       return <div data-testid="test" onClick={handleClick}>{foo}{bar}</div>;
     }
@@ -77,6 +77,45 @@ describe("useState batch scenario", () => {
     unmount();
     expect(renderFn).toBeCalledTimes(2);
     expect(effectFn).toBeCalledTimes(2);
+  })
+
+  it("two set states will trigger only one render with effect check with async handler", async () => {
+    const renderFn = jest.fn();
+    const effectFn = jest.fn();
+    function MyComponent() {
+      const [foo, setFoo] = useState("foo");
+      const [bar, setBar] = useState("bar");
+
+      const handleClick = useCallback(async () => {
+        await new Promise<void>((resolve) => { setFoo("blah"); resolve() })
+        await new Promise<void>((resolve) => { setBar("blah"); resolve() })
+      }, [])
+
+      useEffect(() => {
+        noop(foo);
+        noop(bar);
+        effectFn();
+      }, [foo, bar]);
+      renderFn();
+      return <div data-testid="test" onClick={handleClick}>{foo}{bar}</div>;
+    }
+
+    const { unmount } = render(<MyComponent />);
+    expect(screen.getByTestId("test").textContent).toEqual("foobar");
+    expect(renderFn).toBeCalledTimes(1);
+    expect(effectFn).toBeCalledTimes(1);
+    fireEvent.click(screen.getByTestId("test"))
+    expect(renderFn).toBeCalledTimes(2);
+    expect(effectFn).toBeCalledTimes(2);
+    expect(screen.getByTestId("test").textContent).toEqual("blahbar");
+    // second state update after await
+    await act(() => Promise.resolve());
+    expect(renderFn).toBeCalledTimes(3);
+    expect(effectFn).toBeCalledTimes(3);
+    expect(screen.getByTestId("test").textContent).toEqual("blahblah");
+    unmount();
+    expect(renderFn).toBeCalledTimes(3);
+    expect(effectFn).toBeCalledTimes(3);
   })
 
 
