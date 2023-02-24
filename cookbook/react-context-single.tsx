f52a9a0780f558037df78d55e131838f25598319
Archimedes Trajano <developer@trajano.net>
2023-02-23 23:54:52 -0500

    refactor: relocate stuff into the cookbook

---
+++ cookbook/react-context-single.tsx
@@ -0,0 +1,27 @@
+/**
+ * This provides a boiler plate code for a React context in a single file.  Since
+ * it is a single file the names are intentionally not prefixed except for the final
+ * exports.
+ */
+
+import React, { createContext, PropsWithChildren, useContext, useMemo } from "react";
+
+type ProviderProps = PropsWithChildren<{
+
+}>;
+interface ContextValue {
+
+}
+
+const defaultContextValue: ContextValue = {
+
+}
+
+const Context = createContext<ContextValue>(defaultContextValue);
+
+export const XyzProvider = ({ children }: ProviderProps) => {
+  const contextValue = useMemo<ContextValue>(() => ({}), []);
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>
+}
+
+export const useXyz = () => useContext(Context);
--- src/useStates/useSignal.ts
+++ src/useStates/useSignal.ts
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import noop from "lodash/noop";
+import { Dispatch, useState } from "react";
 
 /**
- * This is like `useState<boolean>` but provides explicit methods to set the value.
- * @returns an array containing the signal value, a setter callback and a reset callback
+ * This is like `useState<boolean>` but provides explicit methods to set the value.  This either sets it to
+ * true or the initial state.  If the intial state is true then this will always be true.
+ * @returns an array containing the signal value, a setter callback and a reset back to initial value callback
  */
 export const useSignal = (
-  initial: boolean = false
+  initial: boolean = false,
+  signalCallback: Dispatch<boolean> = noop,
+  resetCallback: Dispatch<boolean> = noop
 ): [boolean, () => void, () => void] => {
   const [signal, setSignal] = useState(initial);
 
-  return [signal, () => setSignal(true), () => setSignal(false)];
+  return [
+    signal,
+    () => {
+      signalCallback(signal);
+      setSignal(true);
+    },
+    () => {
+      resetCallback(signal);
+      setSignal(initial);
+    },
+  ];
 };
